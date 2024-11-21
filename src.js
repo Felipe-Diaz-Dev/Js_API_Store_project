2b594982aeda64a2054792c357ac263c0380249b
Felipe_Dev <felipediazldev@gmail.com>
2024-11-20 19:25:54 -0500

    testing events and requests

--- src.js
+++ src.js
@@ -1,5 +1,19 @@
 const API_URL =  "https://api.escuelajs.co/api/v1/products"
 
+
+const input_value = document.querySelector("#input-value")
+
+const search_button = document.querySelector("#search-button")
+
+const category_list = document.querySelector("#category-list")
+
+const categories = category_list.children
+console.log(categories)
+
+for (let category of categories) {
+  category.onclick = fecthPosts;
+}
+
 /*
 async function fetchData(){
     const res = await fetch(API_URL)
@@ -9,6 +23,11 @@ async function fetchData(){
 fetchData()
 */    
 
+search_button.addEventListener("click",function(event){
+  event.preventDefault
+  fetch_by_title(input_value.value)
+  console.log(input_value.value)
+})
 
 function sendHTTPRequest(method, url, data) {
     return fetch(url, {
@@ -32,13 +51,12 @@ async function fecthPosts() {
 }
 
 
-let product_name = "Red"
-
-async function fetch_by_title(){
+async function fetch_by_title(product_name){
     const response_title_data = await sendHTTPRequest("GET",`${API_URL}/?title=${product_name}`)
     console.log(response_title_data)
 }
 
 
-fecthPosts()
-fetch_by_title()
+async function fetch_by_category(category){
+  const response_category_data = await sendHTTPRequest("GET",)
+}
